23571c4da0c9b8fd190d52d6bbf891499d3e0cf4
ryayar <yaroslav.ryabikin@gmail.com>
2024-06-08 22:20:34 +0300

    Task 6 complate (.yml or .yaml)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -50,18 +50,6 @@ test('gendiff flat YML files', () => {
   expect(genDiff(filepath1, filepath2)).toBe(expected);
 });
 
-test('gendiff missing keys', () => {
-  const filepath1 = getFixturePath('json/file3.json');
-  const filepath2 = getFixturePath('json/file4.json');
-  const expected = `{
-  - follow: false
-    host: hexlet.io
-  + verbose: true
-}`;
-
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
-});
-
 test('gendiff fail', () => {
   expect(() => genDiff('', '')).toThrow();
 });
