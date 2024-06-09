ab488a259c1583eff7220307d789ecb26611cacc
ryayar <yaroslav.ryabikin@gmail.com>
2024-06-09 21:08:54 +0300

    Refactoring

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -5,12 +5,12 @@ const getKeys = (file0, file1) => _.uniq(_.concat(_.keys(file0), _.keys(file1)))
 const buildDiff = (obj1, obj2) => {
   const keys = _.sortBy(getKeys(obj1, obj2));
   return keys.map((key) => {
-    if (!_.has(obj1, key)) return {name: key, value: obj2[key], status: 'added'};
-    if (!_.has(obj2, key)) return {name: key, value: obj1[key], status: 'deleted'};
+    if (!_.has(obj1, key)) return { name: key, value: obj2[key], status: 'added' };
+    if (!_.has(obj2, key)) return { name: key, value: obj1[key], status: 'deleted' };
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return {name: key, value: buildDiff(obj1[key], obj2[key]), status: 'object'};
+      return { name: key, value: buildDiff(obj1[key], obj2[key]), status: 'object' };
     }
-    if (obj1[key] === obj2[key]) return {name: key, value: obj1[key], status: 'same'};
+    if (obj1[key] === obj2[key]) return { name: key, value: obj1[key], status: 'same' };
     return {
       name: key,
       oldValue: obj1[key],
